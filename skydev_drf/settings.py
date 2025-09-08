b34c4a6d7e8a7b4fb132922a910563e16eb00320
Saps <svsapegin@mail.ru>
2025-09-08 19:51:12 +0300

    static

--- skydev_drf/settings.py
+++ skydev_drf/settings.py
@@ -150,6 +150,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    'templates'
+]
+
+STATIC_ROOT = 'static'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
